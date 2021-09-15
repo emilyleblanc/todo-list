89c0707724cb7461eb872a639c74871ae26c3758
Emily <eleblanc@tvo.org>
2021-09-15 09:23:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ToDoItem from "./ToDoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoInputs: [],
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+
+  }
+  handleInput(e) {
+    if (e.keyCode === 13) {
+      this.setState((prevState) => {
+        return {
+          todoInputs: [
+            ...prevState.todoInputs,
+            {id:prevState.todoInputs.length.toString(), task: e.target.value, isComplete: false},
+          ],
+        };
+      });
+    }
+  }
+
+  handleCheckBox(id){
+    this.setState((prevState) => {
+        const updatedToDos = prevState.todoInputs.map(todo => {
+          if(todo.id === id){
+            todo.isComplete = !todo.isComplete
+          }else{
+            return todo
+          }
+        })
+        console.log("updatedToDos:", updatedToDos)
+        return {
+          todoInputs: updatedToDos
+        }
+    })
+
+    
+  }
+
+  render() {
+    console.log(this.state)
+    const toDoList = this.state.todoInputs.map(todo =>
+      <ToDoItem
+        task={todo.task}
+        id={todo.id}
+        isComplete={todo.isComplete}
+        handleCheckBox ={this.handleCheckBox}
+      />
+    );
+
+    return (
+      <div>
+        <label htmlFor="todo">To do:</label>
+        <input type="text" onKeyDown={this.handleInput} />
+        {toDoList}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ToDoItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ToDoItem extends Component {
+  render() {
+    const completedStyles = {
+      textDecoration: "line-through",
+      fontStyle: "italic",
+    };
+    return (
+      <div key={this.props.id}>
+        <input
+          type="checkbox"
+          checked={this.props.isComplete}
+          onChange={() => this.props.handleCheckBox(this.props.id)}
+        />
+        <p style={this.props.isComplete ? completedStyles : null}>{this.props.task}</p>
+      </div>
+    );
+  }
+}
+
+export default ToDoItem;
